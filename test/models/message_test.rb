f69c2050a6bea6eecd2a11cf0a2f9ee6eb9c3203
DungHo <dung.hoduy@asnet.com.vn>
2017-01-16 16:54:44 +0700

    Add user to chat message

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class MessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "message attributes must not be empty" do
+  	message = Message.new
+  	assert message.invalid?
+  	assert message.errors[:content].any?
+  	assert message.errors[:user_id].any?
+  end
 end
